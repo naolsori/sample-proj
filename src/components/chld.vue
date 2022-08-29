fa9957ce15b992c473a3d7c63698004e809c3f5f
Naol Sori <105045117+naolsori@users.noreply.github.com>
2022-08-29 14:32:57 -0700

    first commit

---
+++ src/components/chld.vue
@@ -0,0 +1,12 @@
+<template>
+<h1> {{name}} </h1>
+</template>
+<script>
+export default{
+    name: 'chld',
+    props: {
+     name
+    }
+}
+
+</script>
---
+++ src/components/part.vue
@@ -0,0 +1,39 @@
+<template>
+    <input type="text" v-model="Data" />
+   <h1>{{Data}}</h1>
+   <ul> <li v-for="item in technology" :key="item"> {{item}} </li>
+</ul>
+<br /><br />
+<ul><li v-for="item in users" :key="item.namme"> name : {{item.name}} and email : {{item.email}}
+</li></ul>
+<chld name="biruk" />
+   </template>
+   <script>
+ import chld from './chld.vue'
+   export default {
+    name: 'part',
+    components:{
+        chld
+    }, 
+    data() {
+       return{
+          technology: [
+            "php",
+            "node",
+            "html"
+            ], 
+            users: [{
+                name: "nah",
+                email: "nahi@test.com"
+            }]
+
+        }
+    }
+}
+       
+   </script>
+   <style scoped>h1{
+   color: orange
+   }
+   
+   </style>
